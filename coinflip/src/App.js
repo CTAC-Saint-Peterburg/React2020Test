65749acacd65db4465c7b155fce5a5618d560a31
Стас <seleznev-00@mail.ru>
2020-12-05 19:03:30 +0300

    first version

--- coinflip/src/App.js
+++ coinflip/src/App.js
@@ -1,61 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-      <div>
-    <div className="mainBlock">
-      <div className="balance">
-        <h3>Общий баланс</h3>
+    <div>
+      <div className="mainBlock">
+        <div className="balance">
+          <h3>Общий баланс</h3>
           <h1>500₽</h1>
           <button className="">Demo/основной</button>
-      </div>
+        </div>
         <div className="avatar">
-            <img className="" src=""></img>
-                <h3>Никнейм</h3>
+          <img className="" src=""></img>
+          <h3>Никнейм</h3>
         </div>
         <div className="stavki">
-            <button className="">текущие ставки</button>
+          <button className="">текущие ставки</button>
         </div>
-    </div>
-          <div class="buttonblock">
-              <div class="blockimg"></div>
-              <p><a href="">история пари</a></p>
       </div>
-    <div className="buttonblock">
+      <div class="buttonblock">
+        <div class="blockimg"></div>
+        <p>
+          <a href="">история пари</a>
+        </p>
+      </div>
+      <div className="buttonblock">
         <div className="blockimg"></div>
-        <p><a href="">снять/пополнить</a></p>
-    </div>
-    <div className="buttonblock">
+        <p>
+          <a href="">снять/пополнить</a>
+        </p>
+      </div>
+      <div className="buttonblock">
         <div className="blockimg"></div>
-        <p><a href="">настройки</a></p>
+        <p>
+          <a href="">настройки</a>
+        </p>
+      </div>
+      <div className="buttonblock">
+        <div className="blockimg"></div>
+        <p>
+          <a href="">создать пари</a>
+        </p>
+      </div>
+      <div className="mainEvents">
+        <div>
+          <button className="buttonOpen">открыть</button>
         </div>
-    <div className="buttonblock">
-        <div className="blockimg"></div>
-        <p><a href="">создать пари</a></p>
-    </div>
-          <div className="mainEvents">
-              <div>
-                  <button className="buttonOpen">открыть</button>
-              </div>
-              <div>
-                  <input className="search" value="поиск"/>
-              </div>
-              <div className="events">
-                  <div>
-                      <img className="eventsImg"/>
-                      <h3>Никнейм</h3>
-                  </div>
-                  <div className="eventsInfo">
-                      <h1>VS</h1>
-                      <h5>110₽</h5>
-                  </div>
-                  <div className="accept">
-                      <button>принять</button>
-                  </div>
-              </div>
+        <div>
+          <input className="search" value="поиск" />
+        </div>
+        <div className="events">
+          <div className="leftBlock">
+            <img className="eventsImg" />
+            <h3>Никнейм</h3>
           </div>
+          <div className="eventsInfo">
+            <h1>VS</h1>
+            <h5>110₽</h5>
+          </div>
+          <div className="accept">
+            <p>Победа:</p>
+            <img/>
+            <p>Поражение:</p>
+            <img/>
+            <button>принять</button>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
 
